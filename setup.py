55ab37932fdc2fb115712cf238e4d34212c48a6d
Nishant Nayudu <nishant@shrimpy.io>
2019-08-22 10:53:35 -0700

    Bumped version to 0.0.6 for websocket release

--- setup.py
+++ setup.py
@@ -13,7 +13,7 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="shrimpy_python",
-    version="0.0.5",
+    version="0.0.6",
     author="ShrimpyOfficial",
     author_email="support@shrimpy.io",
     description="The Official Shrimpy API Python Client",
