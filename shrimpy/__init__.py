9b18c25eb58f25ce3214df57567c4b2a9858fd78
gorkem2020 <gorkem.erdogan@outlook.com>
2021-05-30 16:40:32 +0300

    deleted a duplicate line

--- shrimpy/__init__.py
+++ shrimpy/__init__.py
@@ -1,6 +1,5 @@
 from shrimpy.shrimpy_api_client import *
 from shrimpy.shrimpy_ws_client import *
-from shrimpy.strategy import *
 from shrimpy.portfolio import *
 from shrimpy.strategy import *
 from shrimpy.allocation import *
